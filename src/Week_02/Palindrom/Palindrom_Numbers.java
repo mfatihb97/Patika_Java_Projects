858e0c5404eede5014711bc6eab543fe336dec21
Fatih Batur <88513762+mfatihb97@users.noreply.github.com>
2023-09-18 18:44:34 +0300

    Update Palindrom_Numbers.java

--- src/Week_02/Palindrom/Palindrom_Numbers.java
+++ src/Week_02/Palindrom/Palindrom_Numbers.java
@@ -1,11 +1,13 @@
+//This project finds out if a number you enter same with it's reverse
+
 public class Palindrom_Numbers {
 
     static  boolean isPalindrom(int number){
 
         int temp = number, reverse_of_number=0,last_digit;
         while (temp != 0){
-            last_digit = temp%10;
-            reverse_of_number = (reverse_of_number*10)+last_digit;
+            last_digit = temp%10;                                  // this line of code helps us to get 1st digit
+            reverse_of_number = (reverse_of_number*10)+last_digit; // this line returns 1st digit of number entered and gets it to head (f.e 647 mod 10 = 7 then * 10 70 + last_digit)
             temp/=10;
         }
 
