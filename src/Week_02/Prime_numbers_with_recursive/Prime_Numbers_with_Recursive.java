2b9317f7cd2b013dd4db35d71d07590e0f8c6ec8
fatih batur <mfatihbatur@gmail.com>
2023-09-19 14:58:52 +0300

    Prime_Numbers_with_Recursive

--- src/Week_02/Prime_numbers_with_recursive/Prime_Numbers_with_Recursive.java
+++ src/Week_02/Prime_numbers_with_recursive/Prime_Numbers_with_Recursive.java
@@ -5,22 +5,29 @@ import java.util.Scanner;
 public class Prime_Numbers_with_Recursive {
 
 
-    static void isPrime(int a){
+    static boolean isPrime(int number  ,int divisor){
 
-        if (a==2 || a==3 || a == 5 || a ==7){            // they are prime numbers we exclude them
-            System.out.println(a+" is prime.");
-        }else if(a%2==0 || a%3==0 || a%5==0 || a%7==0){ // least commons for general
-            System.out.println(a+" is not prime.");
-        }else {
-            System.out.println(a+" is prime.");
+        if (number==divisor){
+            return true;
         }
-    }
 
+        if (number%divisor==0){
+            return false;
+        }
+        else {
+            return  isPrime(number,divisor+1);
+        }
+
+    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a number to check if it's prime or not : ");
-        int a = input.nextInt();
-        isPrime(a);
+        int number = input.nextInt();
+        if (isPrime(number, 2)){
+            System.out.println(number+" is prime.");
+        }else {
+            System.out.println(number+" is not prime");
+        }
     }
 }
