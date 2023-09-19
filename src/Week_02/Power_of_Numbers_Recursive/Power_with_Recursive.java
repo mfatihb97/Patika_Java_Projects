c227a3343da660b2c21ed749263346f7b255abed
Fatih Batur <88513762+mfatihb97@users.noreply.github.com>
2023-09-19 09:59:16 +0300

    Power_with_Recursive.java

--- src/Week_02/Power_of_Numbers_Recursive/Power_with_Recursive.java
+++ src/Week_02/Power_of_Numbers_Recursive/Power_with_Recursive.java
@@ -1,25 +1,25 @@
-package Week_02.Power_of_Numbers_Recursive;
-import java.util.Scanner;
+//this project help us to better understand recursive method and calculates power of numbers enter by user
 
+import java.util.Scanner;
 public class Power_with_Recursive{
 
-    static void exponent(){
-
-        Scanner input = new Scanner(System.in);
+    static void exponent(){                        //static called because main method is also static
+        
+        Scanner input = new Scanner(System.in);    
         System.out.println("Enter a number :");
         int n1 = input.nextInt();
         System.out.println("Enter exponent of the number :");
         int n2 = input.nextInt();
         int result=1;
 
-        for (int i=1;i<=n2;i++){
+        for (int i=1;i<=n2;i++){                  //exponent calculation
             result*=n1;
         }
         System.out.println("Result is : "+result);
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {    //simply print our method
         exponent();
 
     }
