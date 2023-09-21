3d362e299c520fab6f8da4aee84c863930a228ce
Fatih Batur <88513762+mfatihb97@users.noreply.github.com>
2023-09-21 17:03:09 +0300

    Sorting_New_List.java

--- src/Week_02/Sorting_New_List/Sorting_New_List.java
+++ src/Week_02/Sorting_New_List/Sorting_New_List.java
@@ -1,5 +1,5 @@
 package Week_02.Sorting_New_List;
-
+//this program allows you to sort the numbers you entered
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,17 +7,17 @@ public class Sorting_New_List {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Length of array : ");
+        System.out.println("Length of array : ");            
         int x = input.nextInt();
         int[] list = new int[x];
 
-        for (int i = 0; i < x; i++) {
+        for (int i = 0; i < x; i++) {                                    // here we need a loop to get inputs of select and attach it to our list
             System.out.println("Enter number " + (i + 1) + " :");
             int n = input.nextInt();
             list[i] = n;
         }
-        System.out.println("Order : ");
-        Arrays.sort(list);
+        System.out.println("Order : ");                                 // here used sort method to sort and print our array
+        Arrays.sort(list);            
         System.out.println(Arrays.toString(list));
 
 
